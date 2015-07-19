7f670c1b834b3474aa09c3d9f326dc1f1468d5b1
howbigbobo <whatagoodmail@gmail.com>
2015-07-19 19:32:05 +0800

    init

---
+++ app/js/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/* App Module */
+
+var phonecatApp = angular.module('phonecatApp', [
+  'ngRoute',
+  'phonecatAnimations',
+
+  'phonecatControllers',
+  'phonecatFilters',
+  'phonecatServices'
+]);
+
+phonecatApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/phones', {
+        templateUrl: 'partials/phone-list.html',
+        controller: 'PhoneListCtrl'
+      }).
+      when('/phones/:phoneId', {
+        templateUrl: 'partials/phone-detail.html',
+        controller: 'PhoneDetailCtrl'
+      }).
+	  when('/meeting-room-reservation', {
+        templateUrl: 'partials/meeting-room-reservation.html',
+        controller: 'MeetingRoomreservation'
+      }).
+      otherwise({
+        redirectTo: '/phones'
+      });
+  }]);
