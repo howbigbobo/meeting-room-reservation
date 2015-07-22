02b28f454663e8de8c39e688b74270a262dd037d
bowen.zhang <bowen.zhang@suryani.cn>
2015-07-22 17:48:30 +0800

    node-router

--- app/app-server.js
+++ app/app-server.js
@@ -1,5 +1,5 @@
 var server = require('../node_modules/node-router/lib/node-router').getServer();
-var staticHandler = new require('../node_modules/node-router/lib/node-router').staticHandler("/","");
+
 /*
 server.get("/json", function (req, res, match) {
   return {hello: "World"};
@@ -9,6 +9,5 @@ server.get(new RegExp("^/(.*)$"), function hello(req, res, match) {
   return "Hello " + (match || "World") + "!";
 });
 */
-server.get(new RegExp("/\.(html|js|css|htm|jpg|jpeg|png|gif)/"),staticHandler);
 
 server.listen(8080);
