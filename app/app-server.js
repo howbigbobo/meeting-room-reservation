af7876dbd0a30712e09721567a37e559d6562437
howbigbobo <whatagoodmail@gmail.com>
2015-07-29 22:33:22 +0800

    controller

--- app/app-server.js
+++ app/app-server.js
@@ -1,7 +1,6 @@
 //var server = require('../node_modules/node-router/lib/node-router').getServer();
 var server = require('node-router').getServer();
-var dateTime = require('./node/date-time');
-var dao = require('./node/dao').dao();
+var controller = require("./node/controller").controller();
 
 /*
 server.get("/json", function (req, res, match) {
@@ -13,28 +12,11 @@ server.get(new RegExp("^/(.*)$"), function hello(req, res, match) {
 });
 */
 var fs = require('fs');
-server.get("/hello",function(req,res,match){
 
-	console.log("--------------------------------");
+server.get("/user/get",controller.getUser);
+server.get("/user/login",controller.login);
+server.get("/user/add",controller.addUser);
 
-	var ip = req.headers['x-forwarded-for'] ||
-		req.connection.remoteAddress ||
-		req.socket.remoteAddress ||
-		req.connection.socket.remoteAddress;
-	console.log(ip);
 
-	dao.add("user", {name:"bowen",ip:123,mac:2222,createDate :(new Date()).format()});
-	dao.find("user",function(err,rows){
-		if(err) console.log(err);
-		else{
-			if(rows && rows.length > 0){
-				for(var i = 0;i < rows.length; i++){
-					console.log(rows[i]);
-				}
-			}
-		}
-	});
-	return {hello: "World"};
-});
 
 server.listen(8080);
--- app/node/controller.js
+++ app/node/controller.js
@@ -1,1 +1,34 @@
+var service = require("./service");
+var userService = service.userService();
+var roomService = service.roomService();
+var reservationService = service.reservationService();
 
+exports.controller = function () {
+    var me = {};
+
+    me.getUser = function (req, res, match) {
+
+    };
+
+    me.login = function (req, res, match) {
+
+    };
+
+    me.addUser = function (req, res, match) {
+
+    };
+
+    me.getReservations = function (req, res, match) {
+
+    };
+
+    me.addReservation = function (req, res, match) {
+
+    };
+
+    me.removeReservation = function(req,res,match){
+
+    };
+
+    return me;
+};
